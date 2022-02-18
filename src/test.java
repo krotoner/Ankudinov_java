9fa898b744d9e3ea618db881af595e00c86b99b2
krot <Kedorr@ya.ru>
2022-02-18 19:27:40 +0300

    update

--- src/test.java
+++ src/test.java
@@ -1,7 +1,24 @@
 class App {
+    public char [][]imege;
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        String[][] imege= new String[8][8];
+        for (int i = 0; i < imege.length; i++) {
+            for (int j = 0; j < imege[0].length; j++) {
+                imege[i][j] = "W";
+
+            }
+        }
+        for (int i = 0; i < imege.length; i++) {
+            String str = "";
+            for (int j = 0; j < imege[0].length; j++)
+                str += imege[i][j];
+            System.out.println(str);
+
+        }
     }
+
 
 }
