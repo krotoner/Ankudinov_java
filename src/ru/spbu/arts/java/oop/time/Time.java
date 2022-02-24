58102309d824fbbd8b73240cac707bf00847f9be
krot <Kedorr@ya.ru>
2022-02-24 13:46:54 +0300

    Созднан отдельный клас для проверки ExperimentsWithTime. удалена проверка из класса Time. Добавлено в каталог пакета.

--- src/ru/spbu/arts/java/oop/time/Time.java
+++ src/ru/spbu/arts/java/oop/time/Time.java
@@ -1,5 +1,5 @@
 package ru.spbu.arts.java.oop.time;
-class Time {
+public class Time {
 
     // формат времени 24 часа 60 минут
     public int hour;
@@ -103,29 +103,5 @@ class Time {
         String greeting = sayHello();
         String time = show();
         System.out.println(greeting + " сейчас " + time);
-    }
-
-    //просто класс main
-    public static void main(String[] args) {
-        Time breakfast = new Time(9, 25);
-        Time lunch = new Time(14, 00);
-        Time dinner = new Time(18, 34);
-        Time good_higt = new Time(22, 50);
-
-        breakfast.printer();
-        lunch.printer();
-        dinner.printer();
-        good_higt.printer();
-        good_higt.add(30);
-        good_higt.printer();
-
-        //предпологается выдача:
-        //Доброе утро сейчас 9 : 25
-        //Добрый день сейчас 14 : 00
-        //Добрый вечер сейчас 18 : 34
-        //Добрый вечер сейчас 22 : 50
-        //time update 30
-        //Доброй ночи сейчас 23 : 20
-
     }
 }
