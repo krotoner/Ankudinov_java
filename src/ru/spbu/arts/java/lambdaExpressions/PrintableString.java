b70f1c5b10ee849031304e789871f8700acba7ab
krot <Kedorr@ya.ru>
2022-04-07 13:33:23 +0300

    добавлен класс Driving

--- src/ru/spbu/arts/java/lambdaExpressions/PrintableString.java
+++ src/ru/spbu/arts/java/lambdaExpressions/PrintableString.java
@@ -1,15 +1,2 @@
-package ru.spbu.arts.java.lambdaExpressions;
-
-public class PrintableString implements Printable{
-
-    private String str;
-
-    PrintableString(String s) {
-        this.str = s;
-    }
-
-    @Override
-    public void print() {
-        System.out.println(str);
-    }
+package ru.spbu.arts.java.lambdaExpressions;public class PrintableString {
 }
